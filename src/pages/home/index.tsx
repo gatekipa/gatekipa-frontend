5103b79e56834b5f05c1bfb49d67055841c2d759
Fahim Nasir <fahimnasir1993@gmail.com>
2024-07-07 22:08:17 +0500

    Homepage to login redirect

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/auth/login");
+  }, []);
   return (
     <>
       <h1>GateKipa Home Page</h1>
