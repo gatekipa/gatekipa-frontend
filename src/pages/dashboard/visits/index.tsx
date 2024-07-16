b6699cacf58b2f8efd6d2f452b845eee0f152c56
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-07-16 11:26:58 +0500

    feat: display visitors

--- src/pages/dashboard/visits/index.tsx
+++ src/pages/dashboard/visits/index.tsx
@@ -1,11 +1,43 @@
-import { fetchVisitsThunk } from "@/app/features/company/thunk";
-import { useAppDispatch } from "@/app/hooks";
+import { fetchVisitsThunk, IVisit } from "@/app/features/company/thunk";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import PaginatedTable from "@/components/shared/paginatedTable";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowBigLeft, ArrowLeft } from "lucide-react";
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+const columns: ColumnDef<IVisit>[] = [
+  {
+    accessorKey: "purposeOfVisit",
+    header: "Purpose of Visit",
+  },
+  {
+    accessorKey: "personToMeet",
+    header: "Person to Meet",
+  },
+  {
+    accessorKey: "personToMeetEmail",
+    header: "Person to Meet's Email",
+  },
+  {
+    accessorKey: "personToMeetMobileNo",
+    header: "Person to Meet's Mobile",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+    cell: ({ getValue }) => {
+      const createdAt = getValue() as Date;
+      return <span>{new Date(createdAt).toLocaleString()}</span>;
+    },
+  },
+];
 
 const VisitsPage: React.FC = () => {
   const dispatch = useAppDispatch();
   const { visitorId } = useParams<{ visitorId: string }>();
+
+  const { visits } = useAppSelector((state) => state.company);
 
   useEffect(() => {
     if (!visitorId) return;
@@ -15,7 +47,15 @@ const VisitsPage: React.FC = () => {
 
   return (
     <div>
-      <h1>Visists Page</h1>
+      <h1 className="flex items-center gap-x-3">
+        <Link to="/dashboard/visitors">
+          <ArrowLeft size={16} />
+        </Link>
+        Visits Page
+      </h1>
+      <div>
+        <PaginatedTable data={visits} columns={columns} />
+      </div>
     </div>
   );
 };
