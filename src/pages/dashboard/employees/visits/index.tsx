87c068435aec7b373b7a6e5dd7f0fa6040dfb483
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-07-27 14:15:27 +0500

    feat: add employee details in visit page

--- src/pages/dashboard/employees/visits/index.tsx
+++ src/pages/dashboard/employees/visits/index.tsx
@@ -28,13 +28,63 @@ const EmployeeVisitsPage: React.FC = () => {
             onClick={() => navigate(`/dashboard/employees`)}
             className='cursor-pointer'
           />
-          <h2 className='text-2xl font-semibold'>
-            Employee Visits {employeeId}
-          </h2>
+          <h2 className='text-2xl font-semibold'>Employee Visits</h2>
         </div>
       </CardContent>
-      <div>
-        <div>{employee?.firstName}</div>
+      <div className='flex justify-between mx-5 md:mx-10'>
+        <div className='space-y-3'>
+          <div className='flex gap-x-8 items-center'>
+            <div className='space-y-1'>
+              <div className='text-xs'>Name</div>
+              <div className='text-sm font-semibold'>
+                {employee?.firstName} {employee?.lastName}
+              </div>
+            </div>
+
+            <div className='space-y-1'>
+              <div className='text-xs'>Employee No</div>
+              <div className='text-sm font-semibold'>
+                {employee?.employeeNo}
+              </div>
+            </div>
+
+            <div className='space-y-1'>
+              <div className='text-xs'>Email Address</div>
+              <div className='text-sm font-semibold'>
+                {employee?.emailAddress}
+              </div>
+            </div>
+
+            <div className='space-y-1'>
+              <div className='text-xs'>Mobile No</div>
+              <div className='text-sm font-semibold'>{employee?.mobileNo}</div>
+            </div>
+          </div>
+          <div className='flex gap-x-8 items-center'>
+            <div className='space-y-1'>
+              <div className='text-xs'>Designation</div>
+              <div className='text-sm font-semibold'>
+                {employee?.designation}
+              </div>
+            </div>
+
+            <div className='space-y-1'>
+              <div className='text-xs'>Shift</div>
+              <div className='text-sm font-semibold'>
+                {employee?.shift?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {/* Get Image Here */}
+          <img
+            src='https://img.freepik.com/free-photo/portrait-young-business-man-posing-with-crossed-arms_23-2149206527.jpg'
+            alt='Employee'
+            className='w-36 h-36 rounded-sm object-cover'
+          />
+        </div>
       </div>
     </Card>
   );
