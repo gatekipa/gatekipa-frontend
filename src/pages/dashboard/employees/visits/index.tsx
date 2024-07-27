b1fbea401a49c2e45058e9d77ba724723d214a7d
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-07-27 13:53:10 +0500

    refactor: optimize employee visits page and add employee details card

--- src/pages/dashboard/employees/visits/index.tsx
+++ src/pages/dashboard/employees/visits/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { IEmployee } from '@/app/features/employee/thunk';
+import { useAppSelector } from '@/app/hooks';
+import { Card, CardContent } from '@/components/ui/card';
+import { ArrowLeft } from 'lucide-react';
+import React, { useMemo } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const EmployeeVisitsPage: React.FC = () => {
-  return <>Visits</>;
+  const navigate = useNavigate();
+
+  const { employeeId } = useParams();
+
+  const { employees } = useAppSelector((state) => state.employee);
+
+  const employee = useMemo(() => {
+    return (
+      employees.find((employee) => employee.id === employeeId) ??
+      ({} as IEmployee)
+    );
+  }, [employeeId, employees]);
+
+  return (
+    <Card>
+      <CardContent>
+        <div className='flex items-center gap-x-2 mt-8'>
+          <ArrowLeft
+            strokeWidth={2}
+            onClick={() => navigate(`/dashboard/employees`)}
+            className='cursor-pointer'
+          />
+          <h2 className='text-2xl font-semibold'>
+            Employee Visits {employeeId}
+          </h2>
+        </div>
+      </CardContent>
+      <div>
+        <div>{employee?.firstName}</div>
+      </div>
+    </Card>
+  );
 };
 
 export default EmployeeVisitsPage;
