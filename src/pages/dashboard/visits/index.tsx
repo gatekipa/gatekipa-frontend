83a00b487144efa5b1b9215a73117171847f3e51
Fahim Nasir <fahimnasir1993@gmail.com>
2024-07-29 13:08:45 +0500

    Check added for null in visit employee person to meet

--- src/pages/dashboard/visits/index.tsx
+++ src/pages/dashboard/visits/index.tsx
@@ -1,55 +1,59 @@
-import { fetchVisitsThunk, IVisit } from '@/app/features/company/thunk';
-import { fetchEmployeesThunk } from '@/app/features/employee/thunk';
-import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import { fetchVisitsThunk, IVisit } from "@/app/features/company/thunk";
+import { fetchEmployeesThunk } from "@/app/features/employee/thunk";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import ConfirmModal, {
   ModalType,
-} from '@/components/features/visits/confirmModal';
-import VisitsToolbar from '@/components/features/visits/toolbar';
-import ColumnHeader from '@/components/shared/columnHeader';
-import PaginatedTable from '@/components/shared/paginatedTable';
-import { Card, CardContent } from '@/components/ui/card';
-import { formatDate } from '@/utils';
-import { ColumnDef } from '@tanstack/react-table';
-import { ArrowLeft } from 'lucide-react';
-import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+} from "@/components/features/visits/confirmModal";
+import VisitsToolbar from "@/components/features/visits/toolbar";
+import ColumnHeader from "@/components/shared/columnHeader";
+import PaginatedTable from "@/components/shared/paginatedTable";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatDate } from "@/utils";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowLeft } from "lucide-react";
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const columns: ColumnDef<IVisit>[] = [
   {
-    accessorKey: 'purposeOfVisit',
+    accessorKey: "purposeOfVisit",
     header: ({ column }) => (
-      <ColumnHeader column={column} label='Purpose of Visit' />
+      <ColumnHeader column={column} label="Purpose of Visit" />
     ),
     enableSorting: true,
   },
   {
-    accessorKey: 'employee',
+    accessorKey: "employee",
     header: ({ column }) => (
-      <ColumnHeader column={column} label='Person to Meet' />
+      <ColumnHeader column={column} label="Person to Meet" />
     ),
     enableSorting: true,
     cell: ({ row }) => {
-      const { firstName, lastName } = row.original.employee;
-      return `${firstName} ${lastName}`;
+      if (row.original.employee && row.original.employee !== null) {
+        const { firstName, lastName } = row.original.employee;
+        return `${firstName} ${lastName}`;
+      } else {
+        return "N/A";
+      }
     },
   },
   {
-    accessorKey: 'employee.emailAddress',
+    accessorKey: "employee.emailAddress",
     header: ({ column }) => (
       <ColumnHeader column={column} label="Person to Meet's Email" />
     ),
     enableSorting: true,
   },
   {
-    accessorKey: 'employee.mobileNo',
+    accessorKey: "employee.mobileNo",
     header: ({ column }) => (
       <ColumnHeader column={column} label="Person to Meet's Mobile" />
     ),
     enableSorting: true,
   },
   {
-    accessorKey: 'visitDate',
-    header: ({ column }) => <ColumnHeader column={column} label='Visit Date' />,
+    accessorKey: "visitDate",
+    header: ({ column }) => <ColumnHeader column={column} label="Visit Date" />,
     enableSorting: true,
     cell: ({ getValue }) => {
       const createdAt = getValue() as Date;
@@ -61,9 +65,9 @@ const columns: ColumnDef<IVisit>[] = [
     },
   },
   {
-    accessorKey: 'checkInTime',
+    accessorKey: "checkInTime",
     header: ({ column }) => (
-      <ColumnHeader column={column} label='Check In Time' />
+      <ColumnHeader column={column} label="Check In Time" />
     ),
     enableSorting: true,
     cell: ({ getValue }) => {
@@ -76,9 +80,9 @@ const columns: ColumnDef<IVisit>[] = [
     },
   },
   {
-    accessorKey: 'checkoutTime',
+    accessorKey: "checkoutTime",
     header: ({ column }) => (
-      <ColumnHeader column={column} label='Check Out Time' />
+      <ColumnHeader column={column} label="Check Out Time" />
     ),
     enableSorting: true,
     cell: ({ getValue }) => {
@@ -91,8 +95,8 @@ const columns: ColumnDef<IVisit>[] = [
     },
   },
   {
-    id: 'actions',
-    header: 'Actions',
+    id: "actions",
+    header: "Actions",
     cell: ({ row }) => {
       const visit = row.original;
       const currentDate = new Date();
@@ -101,17 +105,17 @@ const columns: ColumnDef<IVisit>[] = [
       const visitCheckInDate = new Date(visit.checkInTime);
       visitCheckInDate.setHours(0, 0, 0, 0);
       return (
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           {!visit.checkInTime && (
             /*visitCheckInDate >= currentDate &&*/ <ConfirmModal
-              label='Check In'
+              label="Check In"
               type={ModalType.CHECK_IN}
               visit={visit}
             />
           )}
           {!visit.checkoutTime && (
             <ConfirmModal
-              label='Check Out'
+              label="Check Out"
               type={ModalType.CHECK_OUT}
               visit={visit}
             />
@@ -134,7 +138,7 @@ const VisitsPage: React.FC = () => {
 
     dispatch(
       fetchVisitsThunk({
-        visitorId: visitorId !== ':visitorId' ? visitorId! : user?.visitorId!,
+        visitorId: visitorId !== ":visitorId" ? visitorId! : user?.visitorId!,
       })
     );
 
@@ -144,17 +148,17 @@ const VisitsPage: React.FC = () => {
   return (
     <Card>
       <CardContent>
-        <div className='flex items-center justify-between mt-6'>
-          <h1 className='flex items-center gap-x-3 text-2xl font-semibold'>
-            {user?.userType !== 'VISITOR' && (
-              <Link to='/dashboard/visitors'>
+        <div className="flex items-center justify-between mt-6">
+          <h1 className="flex items-center gap-x-3 text-2xl font-semibold">
+            {user?.userType !== "VISITOR" && (
+              <Link to="/dashboard/visitors">
                 <ArrowLeft size={16} />
               </Link>
             )}
             Visits Page
           </h1>
           <div>
-            <VisitsToolbar visitorId={visitorId!} mode='toolbar' />
+            <VisitsToolbar visitorId={visitorId!} mode="toolbar" />
           </div>
         </div>
         <div>
