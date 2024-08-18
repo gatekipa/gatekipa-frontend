030b23285f3630b31e78aa35b176d0749ce75ca9
Umer Nasir <umer.nas@outlook.com>
2024-08-18 17:02:04 +0500

    feat: redirect to pricing if no plan is selected

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,10 +2,18 @@ import EmployeeDashboard from "@/components/features/employees/dashboard";
 import VisitorDashboard from "@/components/features/visitors/dashboard";
 import { UserRole } from "@/constants/enums";
 import { getUserPlan, getUserRole } from "@/utils";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage: React.FC = () => {
   const role = getUserRole();
-  const userPlan = getUserPlan();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userPlan = getUserPlan();
+
+    if (userPlan && !userPlan?.isSubscriptionActive) navigate("/pricing");
+  }, []);
 
   return (
     <div>
