1d15ea4ecab0264f3d5c6fc639716d32b1c878c2
Umer Nasir <umer.nas@outlook.com>
2024-08-18 12:20:36 +0500

    fix: resolve bug for not allowed features visibility

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,5 +1,6 @@
 import { IPlan } from "@/app/features/pricing/thunk";
 import usePricingPlans from "@/hooks/pricing";
+import { X } from "lucide-react";
 import React from "react";
 
 const PlanCard: React.FC<{
@@ -42,20 +43,8 @@ const PlanCard: React.FC<{
                   </div>
                 ) : (
                   <div className="flex">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      className="w-6 h-6 mr-2 font-bold text-green-600"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
-                        clipRule="evenodd"
-                      ></path>
-                      {detail.text}
-                    </svg>
+                    <X size={24} className="mr-2 font-bold text-red-600" />
+                    {detail.text}
                   </div>
                 )}
               </p>
