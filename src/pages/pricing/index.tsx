321d659654545e7f0b9ec54075a55edfb9f052f5
Umer Nasir <umer.nas@outlook.com>
2024-08-18 12:27:29 +0500

    fix: resolve style issues on smaller screens

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -6,7 +6,7 @@ const PricingPage: React.FC = () => {
   const { plans } = usePricingPlans();
 
   return (
-    <div className="h-screen flex justify-center items-center gap-x-8 text-white">
+    <div className="h-screen flex flex-col justify-center items-center gap-8 space-y-16 text-white md:flex-row md:space-y-0">
       {plans.map((plan, index) => (
         <PlanCard key={index} plan={plan} />
       ))}
