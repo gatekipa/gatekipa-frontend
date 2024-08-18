32ec0e4d51bb6aa7f1d014bb860e5c32bdaa42b5
Umer Nasir <umer.nas@outlook.com>
2024-08-18 13:05:49 +0500

    refactor: update registration page to include email verification process

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,16 +1,28 @@
-import { useAppDispatch } from "@/app/hooks";
-import RegistrationForm from "../../../components/features/auth/registrationForm";
 import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import RegistrationForm from "../../../components/features/auth/registrationForm";
 import { fetchCompanyThunk } from "@/app/features/company/thunk";
+import VerifyEmailForm from "@/components/features/auth/verifyEmailForm";
+import VerifyEmailTokenForm from "@/components/features/auth/verifyEmailTokenForm";
 
 const RegistrationPage: React.FC = () => {
   const dispatch = useAppDispatch();
+
+  const {
+    registerUser: { isVerificationEmailSent, isEmailVerified },
+  } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(fetchCompanyThunk());
   }, []);
 
-  return <RegistrationForm />;
+  if (!isVerificationEmailSent) {
+    return <VerifyEmailForm />;
+  } else if (isVerificationEmailSent && !isEmailVerified) {
+    return <VerifyEmailTokenForm />;
+  } else {
+    return <RegistrationForm />;
+  }
 };
 
 export default RegistrationPage;
