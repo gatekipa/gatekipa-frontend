565be4244ade96931c96a2f6f8c3ddf710f58071
Umer Nasir <umer.nas@outlook.com>
2024-08-27 21:30:58 +0500

    feat: resize dashboard tiles for admin

--- src/components/features/company/dashboard/index.tsx
+++ src/components/features/company/dashboard/index.tsx
@@ -10,8 +10,8 @@ const CompanyDashboard: React.FC = () => {
       <h2 className="text-2xl text-gray-950 font-semibold dark:text-white">
         Dashboard
       </h2>
-      <div className="flex flex-col my-5 gap-4 md:flex-row">
-        <div className="flex flex-col gap-y-4 bg-green-700 rounded-md shadow-lg text-white w-1/5 p-5 cursor-pointer transition-opacity dark:bg-green-950 hover:opacity-80">
+      <div className="flex flex-col justify-center items-center my-5 gap-4 md:flex-row md:justify-start md:items-start">
+        <div className="flex flex-col gap-y-4 bg-green-700 rounded-md shadow-lg text-white w-full p-5 cursor-pointer transition-opacity dark:bg-green-950 hover:opacity-80 md:w-1/3">
           <h3 className="text-xl font-semibold">Current Plan</h3>
           <Badge
             variant="secondary"
@@ -20,7 +20,7 @@ const CompanyDashboard: React.FC = () => {
             {data?.plan.planName}
           </Badge>
         </div>
-        <div className="flex flex-col gap-y-4 bg-green-700 rounded-md shadow-lg text-white w-1/5 p-5 cursor-pointer transition-opacity dark:bg-green-950 hover:opacity-80">
+        <div className="flex flex-col gap-y-4 bg-green-700 rounded-md shadow-lg text-white w-full p-5 cursor-pointer transition-opacity dark:bg-green-950 hover:opacity-80 md:w-1/3">
           <h3 className="text-xl font-semibold">Last Payment Date</h3>
           <Badge
             variant="secondary"
@@ -29,7 +29,7 @@ const CompanyDashboard: React.FC = () => {
             {new Date(data?.lastPaymentDate!).toLocaleDateString()}
           </Badge>
         </div>
-        <div className="flex flex-col gap-y-4 bg-green-700 rounded-md shadow-lg text-white w-1/5 p-5 cursor-pointer transition-opacity dark:bg-green-950 hover:opacity-80">
+        <div className="flex flex-col gap-y-4 bg-green-700 rounded-md shadow-lg text-white w-full p-5 cursor-pointer transition-opacity dark:bg-green-950 hover:opacity-80 md:w-1/3">
           <h3 className="text-xl font-semibold">Next Payment Date</h3>
           <Badge
             variant="secondary"
