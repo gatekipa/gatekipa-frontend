c050c9b0eab9b56c1f1dd5cbb3ad98a7ba27525f
Umer Nasir <umer.nas@outlook.com>
2024-09-11 19:42:21 +0500

    refactor: update company logo link to use root path

--- src/components/features/home/companyLogo/index.tsx
+++ src/components/features/home/companyLogo/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import logo from "../../../../assets/logo.svg";
 
 const CompanyLogo: React.FC = () => (
-  <Link className="flex items-center justify-center" to="#">
+  <Link className="flex items-center justify-center" to="/">
     <img src={logo} alt="logo" className="w-8" />
     <span className="ml-2 text-2xl font-bold font-cursive__pacifico">
       Gate Kipa
