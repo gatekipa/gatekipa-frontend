7f6aa177cc4ce88dee3cef00d3907c30447eb5ee
Umer Nasir <umer.nas@outlook.com>
2024-09-14 19:26:06 +0500

    feat: add create pricing page and component

--- src/pages/pricing/create/index.tsx
+++ src/pages/pricing/create/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Card, CardContent } from "@/components/ui/card";
 
 const CreatePricingPage: React.FC = () => {
-  return <div>Pricing Plan</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Card>
+      <div className="flex items-center p-5 gap-4">
+        <ArrowLeft
+          className="cursor-pointer size-6 hover:opacity-70"
+          onClick={() => navigate("/dashboard/pricing")}
+        />
+        <h2 className="text-2xl font-semibold">Create Pricing Plan</h2>
+      </div>
+      <CardContent></CardContent>
+    </Card>
+  );
 };
 
 export default CreatePricingPage;
