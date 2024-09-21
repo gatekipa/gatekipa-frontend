b911dbb784b4b7b3f4a599a6c3d023b20e79f0a9
Umer Nasir <umer.nas@outlook.com>
2024-09-21 15:31:08 +0500

    refactor: Remove unused localStorage values after payment

--- src/app/features/pricing/slice.ts
+++ src/app/features/pricing/slice.ts
@@ -198,7 +198,6 @@ export const pricingSlice = createSlice({
           "userInfo",
           JSON.stringify({ data: action.payload })
         );
-        localStorage.removeItem("selectedPlan");
         localStorage.removeItem("selectedPromotionalPricing");
         state.loading[PricingApiEndpoint.CONFIRM_PAYMENT] = false;
       }
