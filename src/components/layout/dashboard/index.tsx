50061aa59ea2bf2b3b92ac331ba35a0182a9981b
Umer Nasir <umer.nas@outlook.com>
2024-10-18 22:32:00 +0500

    feat: make sidebar collapsible

--- src/components/layout/dashboard/index.tsx
+++ src/components/layout/dashboard/index.tsx
@@ -4,15 +4,22 @@ import { Card, CardContent } from "@/components/ui/card";
 import { mappedRoutes } from "@/constants";
 import { UserRole } from "@/constants/enums";
 import { getUserRole } from "@/utils";
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../../app/hooks";
+import { MenuIcon } from "lucide-react";
+import ToolTip from "@/components/shared/tooltip";
 
 const DashboardLayout: React.FC = () => {
   const navigate = useNavigate();
 
   const user = useAppSelector((state) => state.auth.user);
   const role = getUserRole() as UserRole;
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () =>
+    setIsSidebarOpen((isSidebarOpen) => !isSidebarOpen);
 
   useEffect(() => {
     const role = getUserRole() as UserRole;
@@ -48,9 +55,26 @@ const DashboardLayout: React.FC = () => {
       <div className="flex h-screen flex-col">
         <Navbar />
         <div className="flex-1 grid grid-cols-1 md:grid-cols-6">
-          <Card className="col-span-1 py-4 rounded-none">
+          <Card
+            className={`${
+              isSidebarOpen ? "w-64" : "w-16"
+            } transition-all duration-300 ease-in-out py-4 rounded-none`}
+          >
             <CardContent>
-              <aside>
+              <div
+                className="self-end mb-4 mr-2"
+                onClick={toggleSidebar}
+                aria-label={
+                  isSidebarOpen ? "Collapse sidebar" : "Expand sidebar"
+                }
+              >
+                <MenuIcon className="size-4 text-gray-700 cursor-pointer hover:text-gray-900" />
+              </div>
+              <aside
+                className={`flex flex-col h-full ${
+                  !isSidebarOpen && "justify-center items-center"
+                }`}
+              >
                 {routes?.map((route) => (
                   <NavLink
                     key={route.href}
@@ -64,11 +88,13 @@ const DashboardLayout: React.FC = () => {
                       }`
                     }
                   >
-                    {route.icon}
-                    <span className="transition-all duration-300 ease-in-out hover:underline hover:underline-offset-4">
-                      {" "}
-                      {route.label}
-                    </span>
+                    <ToolTip title={route.label}>{route.icon}</ToolTip>
+
+                    {isSidebarOpen && (
+                      <span className="transition-all duration-300 ease-in-out">
+                        {route.label}
+                      </span>
+                    )}
                   </NavLink>
                 ))}
               </aside>
