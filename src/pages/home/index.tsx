bf63b8be192023f4a2f19fad33af2acdbf870c6e
Fahim Nasir <fahimnasir1993@gmail.com>
2024-10-22 18:31:40 +0500

    Gate Kipa to GateKias renaming

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -104,7 +104,7 @@ const HomePage = () => {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                  Try Gate Kipa for Free Today!
+                  Try GateKipas for Free Today!
                 </h2>
                 <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
                   Start managing your visitors and employees more efficiently.
